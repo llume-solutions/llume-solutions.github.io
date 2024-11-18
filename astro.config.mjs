64b5df7648e9b03421742f91b29d05c30bc67afd
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-11-18 16:08:54 +0000

    Front page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import lottie from "astro-integration-lottie";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), lottie()]
 });
